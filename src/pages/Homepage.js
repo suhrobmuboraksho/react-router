218cb4ca8654eda8f8eb60c8eae015a31221aeec
Sukhrob <suhrob.m89@gmail.com>
2023-01-22 11:00:54 -0800

    Added NavLink and tried programmatic navigation.

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Homepage = () => {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate("/products");
+  };
   return (
     <>
       <h1>Homepage</h1>
       <p>
         Go to <Link to="/products">the list of products.</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
